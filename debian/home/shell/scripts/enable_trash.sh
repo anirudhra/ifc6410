bf73b72b97fd10b66c80b33492a453a2707592f9
Anirudh Acharya <anirudhra@users.noreply.github.com>
2022-08-14 17:50:45 -0700

    Delete debian/home/shell/scripts directory

--- debian/home/shell/scripts/enable_trash.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-mkdir --parents $HOME/.local/share/file-manager/actions
-wget -O $HOME/.local/share/file-manager/actions/ask-trash-empty.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/lubuntu/trash-empty/ask-trash-empty.desktop
-sudo wget -O /usr/local/bin/ask-trash-empty https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/lubuntu/trash-empty/ask-trash-empty.desktop
-sudo chmod a+x /usr/local/bin/ask-trash-empty
-
