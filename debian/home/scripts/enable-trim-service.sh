cda7d0c278d2fc7905675bd18a3f16a5f097652e
Anirudh Acharya <anirudhra@users.noreply.github.com>
2023-12-31 13:14:55 -0800

    Delete debian/home directory

--- debian/home/scripts/enable-trim-service.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-sudo cp /usr/share/doc/util-linux/examples/fstrim.service /etc/systemd/system
-sudo cp /usr/share/doc/util-linux/examples/fstrim.timer /etc/systemd/system
-sudo systemctl enable fstrim.timer
-
--- debian/home/scripts/enable_trash.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-sudo apt-get install trash-cli
-mkdir --parents $HOME/.local/share/file-manager/actions
-wget -O $HOME/.local/share/file-manager/actions/ask-trash-empty.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/lubuntu/trash-empty/ask-trash-empty.desktop
-sudo wget -O /usr/local/bin/ask-trash-empty https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/lubuntu/trash-empty/ask-trash-empty.desktop
-sudo chmod a+x /usr/local/bin/ask-trash-empty
-
