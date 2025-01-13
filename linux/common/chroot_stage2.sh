98f0c2d5cbc014c58f803d22e1369babcb0547fc
anirudhra <anirudhra@hotmail.com>
2025-01-13 13:24:05 -0800

    Added common and debian linux specific files Added chroot stage2 script for bootstraping rootfs

---
+++ linux/common/chroot_stage2.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# directory where the new bootstrap rootfs is mounted
+ROOTFS="/mnt/rootfs"
+
+# create stage 2 sys directories
+mount --make-rslave --rbind /proc ${ROOTFS}/proc
+mount --make-rslave --rbind /sys ${ROOTFS}/sys
+mount --make-rslave --rbind /dev ${ROOTFS}/dev
+mount --make-rslave --rbind /run ${ROOTFS}/run
+
+# copy some skeletal files to get started
+cp /etc/fstab ${ROOTFS}/etc/fstab
+cp -a /etc/ssh/sshd_config ${ROOTFS}/etc/ssh/sshd_config
+cp -a /etc/ssh/ssh_config ${ROOTFS}/etc/ssh/ssh_config
+cp -a /etc/NetworkManager ${ROOTFS}/etc/NetworkManager
+
+# copy included files in this repo's /etc to rootfs
+cp -a ./etc/* ${ROOTFS}/etc/
+
+# chroot to new install
+chroot ${ROOTFS}
+
+# install following packages at the very least
+# btop ssh ca-certificates tmux duf nano sudo console-setup console-setup-linux network-manager wget curl lsb-release
+# configure: dpkg-reconfigure tzdata, dpkg-reconfigure locales
