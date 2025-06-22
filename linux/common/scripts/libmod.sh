3d6f97fafe5cc7a19fdce5d3cc9d725703aab172
anirudhra <anirudhra@hotmail.com>
2025-06-22 15:50:05 -0700

    Fixed module copy-extract script

--- linux/common/scripts/libmod.sh
+++ linux/common/scripts/libmod.sh
@@ -2,7 +2,7 @@
 
 cd /lib/modules
 cp /mnt/nfs/sata-ssd/ssd-data/backup/ifc6410/github/poky/build/qcom-armv7a/artifacts/modules* .
-tar xvf modules-ifc6410-armv7a-6.6.9-20250621.tgz
+tar xvf modules*.tgz
 cd ./lib/modules
 mv 6.6* /lib/modules
 cd /lib/modules
